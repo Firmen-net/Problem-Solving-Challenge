d23879fd7c29c3a9bee5cdf3548382d00b610c29
Firmen-net <maduragame1810@gmail.com>
2021-12-26 22:50:51 +0700

    initial

--- Assets/control.cs
+++ Assets/control.cs
@@ -11,7 +11,10 @@ public class control : MonoBehaviour
    
 
     private Rigidbody2D rb;
-    public float speed = 0.01f;
+    public float speed = 1f;
+
+    private Vector3 mousePosition = Input.mousePosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,23 +26,25 @@ public class control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
 
-
-        if (Input.GetKey(upButton))
-        {
-            rb.transform.Translate(0, speed, Time.deltaTime);
-        }
-         if (Input.GetKey(rightButton))
-        {
-            rb.transform.Translate(speed, 0, Time.deltaTime);
-        }
-         if (Input.GetKey(leftButton))
-        {
-            rb.transform.Translate(-speed, 0, Time.deltaTime);
-        }
-         if (Input.GetKey(downButton))
-        {
-            rb.transform.Translate(0, -speed, Time.deltaTime);
-        }
+         /* if (Input.GetKey(upButton))
+          {
+              rb.transform.Translate(0, speed, Time.deltaTime);
+          }
+           if (Input.GetKey(rightButton))
+          {
+              rb.transform.Translate(speed, 0, Time.deltaTime);
+          }
+           if (Input.GetKey(leftButton))
+          {
+              rb.transform.Translate(-speed, 0, Time.deltaTime);
+          }
+           if (Input.GetKey(downButton))
+          {
+              rb.transform.Translate(0, -speed, Time.deltaTime);
+          }*/
+         
+        rb.transform.position = Vector2.MoveTowards(rb.transform.position, mousePosition,speed*Time.deltaTime);
     }
 }
