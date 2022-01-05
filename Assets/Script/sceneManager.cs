81f9cf216a72f041fb428745f67802dc787118d9
Firmen-net <maduragame1810@gmail.com>
2022-01-05 07:16:50 +0700

    update

---
+++ Assets/Script/sceneManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    public void Scene1()
+    {
+        SceneManager.LoadScene("Problem 1");
+    }
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("Problem 2");
+    }
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Problem 3");
+    }
+
+    public void Scene4()
+    {
+        SceneManager.LoadScene("Problem 4");
+    }
+
+    public void Scene5()
+    {
+        SceneManager.LoadScene("Problem 5");
+    }
+
+    public void Scene6()
+    {
+        SceneManager.LoadScene("Problem 6");
+    }
+
+    public void Scene7()
+    {
+        SceneManager.LoadScene("Problem 7");
+    }
+
+    public void Scene8()
+    {
+        SceneManager.LoadScene("Problem 8");
+    }
+
+    public void Scene9()
+    {
+        SceneManager.LoadScene("Problem 9");
+    }
+}
